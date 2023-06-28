cb7a906c61f1421063f57fcd527f34b4de2b288e
Daniel <danielnguy3n12@gmail.com>
2023-06-29 02:53:07 +1000

    Dynamic routing for item details and displaying dynamic data on the page

--- src/components/home/HotCollections.jsx
+++ src/components/home/HotCollections.jsx
@@ -93,7 +93,7 @@ const HotCollections = ({ hotCollections, hotCollectionsLoading }) => {
                   <div key={collection.id}>
                     <div className="nft_coll">
                       <div className="nft_wrap">
-                        <Link to="/item-details">
+                        <Link to={`/item-details/${collection.nftId}`}>
                           <img
                             src={collection.nftImage}
                             className="lazy img-fluid"
