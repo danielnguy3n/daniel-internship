6c0f3282a3d80a2bd5e4a2084a9960a041d53f14
Daniel <danielnguy3n12@gmail.com>
2023-06-29 02:10:58 +1000

    Dynamic nft collection for author, skeleton loading states and follow button

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,9 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
 import Slider from "react-slick";
 import "../../css/styles/slick.css";
 import "../../css/styles/slick-theme.css";
-import Skeleton from "../UI/Skeleton";
 import Item from "../UI/Item";
 import ItemSkeleton from "../UI/ItemSkeleton";
 
@@ -37,47 +35,6 @@ const NewItems = ({ newItems, newItemsLoading }) => {
       },
     ],
   };
-
-  function CountdownTimer(expiryDate) {
-    const timeLeftInSeconds = (expiryDate - Date.now()) / 1000;
-    const startTime = Date.now();
-  
-    const [seconds, setSeconds] = useState(0);
-    const [minutes, setMinutes] = useState(0);
-    const [hours, setHours] = useState(0);
-  
-    function updateTimer() {
-      const secondsElapsed = (Date.now() - startTime) / 1000;
-      const updateCountdown = timeLeftInSeconds - secondsElapsed;
-  
-      setSeconds(Math.floor(updateCountdown) % 60);
-      setMinutes(Math.floor(updateCountdown / 60) % 60);
-      setHours(Math.floor(updateCountdown / (60 * 60)) % 24);
-    }
-  
-    useEffect(() => {
-      const interval = setInterval(() => updateTimer(), 100);
-      return () => clearInterval(interval);
-    }, []);
-  
-    if (timeLeftInSeconds < 0) {
-      return (
-          <div></div>
-      )
-    }
-  
-    return (
-      <div className="de_countdown">
-        {seconds < 0 ? (
-          <span> EXPIRED </span>
-        ) : (
-          <span>
-            {hours}h {minutes}m {seconds}s
-          </span>
-        )}
-      </div>
-    );
-  }
 
   return (
     <section id="section-items" className="no-bottom">
